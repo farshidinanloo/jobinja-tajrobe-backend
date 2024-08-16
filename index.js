00f732f28b5048a3ef637106947ebaab3584107a
farshid inanloo <farshidinanloo73@gmail.com>
2024-08-16 19:05:03 +0330

    first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+import express from 'express';
+import cors from 'cors';
+import puppeteer from 'puppeteer';
+import fs from 'fs';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.listen(3000, () => {
+	console.log('Listening on port 3000!');
+});
+
+app.get('/company', async (req, res) => {
+	const { name } = req.query;
+	const data = await scrape(name);
+
+	res.json(data);
+});
+
+const scrape = async (name) => {
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+
+	const url = 'https://tajrobe.github.io/search';
+
+	await page.goto(url);
+
+	await page.waitForSelector('.hero-search input');
+	await page.type('.hero-search input', name);
+	await page.keyboard.press('Enter');
+
+	await page.waitForSelector('a.black-link', {
+		timeout: 100000,
+	});
+	await page.click('a.black-link');
+
+	const data = await page.evaluate(() => {
+		const cardElements = document.querySelectorAll(
+			'#sorted-section .c-cardText.o-box'
+		);
+		return Array.from(cardElements).map((card) => {
+			const title = card.querySelector('.c-cardText__title').innerText;
+			const date = card.querySelector('time').innerText;
+			const body = card.querySelector('.c-cardText__body').innerText;
+			return {
+				title,
+				date,
+				body,
+			};
+		});
+	});
+	console.log(data);
+	fs.writeFileSync('data.json', JSON.stringify(data, null, 2));
+	await browser.close();
+	return data;
+};
+
